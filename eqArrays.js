e0c1efaad9ee31c41c336807cb82eec24ea00ab8
Chris <46506838+MotorcycleCapitalist@users.noreply.github.com>
2022-08-30 20:58:29 -0600

    eqArrays.js
    Flex Module 1 Week 4

---
+++ eqArrays.js
@@ -0,0 +1,13 @@
+// function implementation
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) 
+  if ([array1] === [array2]) {
+    return true 
+  } else {
+    return false
+  }
+  }
+  
+  
+
+  module.exports = eqArrays;
